22d2df75a0a4475ec6c39169fd0fe4653c892f0f
Phil Bajenski <carpetree@icloud.com>
2025-07-29 02:16:20 -0700

    fix: use absolute paths in admin-notification.php to prevent include path issues

--- server/api/admin-notification.php
+++ server/api/admin-notification.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
-require_once '../config/database-simple.php';
-require_once '../config/config.php';
+require_once __DIR__ . '/../config/database-simple.php';
+require_once __DIR__ . '/../config/config.php';
 
 // Function to send admin notification email with attachments
 function sendAdminNotification($quote_id) {
